dbe683d6e93c639bec75bd716237acbe1ce37a60
nbernstein3 <nbernst3@msudenver.edu>
2022-02-23 13:32:32 -0700

    updates to graphics

--- graphics1.py
+++ graphics1.py
@@ -1,7 +1,18 @@
 from graphics import *
+import time
 def DrawCircle():
-    windw = GraphWin("A Circle", 100,100)
-    windw.setBackground("black")
+    windw = GraphWin("A Circle", 500,500)
+    windw.setBackground("midnight blue")
+    x = 500
+    y = 250
+    while x>0:
+        p = Point(x,y)
+        c = Circle(p,x)
+        c.draw(windw)
+        x-=5
+        c.setFill("light blue")
+        '''time.sleep(.05)
+        c.undraw()'''
     windw.getMouse()
     windw.close()
 
